e1561521b2fb96d52bbcb57b6e0fa40cd1654cfb
Eric <eric91nguyen@yahoo.com>
2024-09-04 19:54:35 -0500

    Secured The Vault

---
+++ index.js
@@ -0,0 +1,21 @@
+// Type a stringVar with quotes
+const stringVar = "You have received this message because you have been chosen to open an important vault. Here is the sercret combination:"
+
+// Added 3 variable of the combination of 10-40-39
+const numberVar = (5 + 5) * 1 - 0;
+const numberVar2 = (5 + 5) * 4 - 0;
+const numberVar3 = (5 + 5) * 4 - 1; 
+
+alert (stringVar); 
+alert (numberVar);
+alert (numberVar2);
+alert (numberVar3);
+
+/**
+ * Pseudocode 
+ * The first variable includes a string with comments inside quotes
+ Setting 3 variables has a combination that is a set of 10-40-39
+ The alert allows the pop-up of the variables written above 
+*/
+
+
